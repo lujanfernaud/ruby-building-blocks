4095994c8d8a455711e5d0a2416734a670cd4e1d
lujanfernaud <lujanfernaud@gmail.com>
2017-03-05 20:08:15 +0000

    Start substrings

---
+++ substrings.rb
@@ -0,0 +1,8 @@
+# Project: Substrings for The Odin Project
+# Author: Luján Fernaud
+#
+# Instructions: Implement a method #substrings that takes a word as the first
+# argument and then an array of valid substrings (your dictionary) as the second
+# argument. It should return a hash listing each substring (case insensitive)
+# that was found in the original string and how many times it was found.
+
