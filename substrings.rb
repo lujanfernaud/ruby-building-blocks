3ef96feba6a7a38513cc2f124da2977fe0e0f0a2
lujanfernaud <lujanfernaud@gmail.com>
2017-03-05 21:53:48 +0000

    Refactor and prettify output

--- substrings.rb
+++ substrings.rb
@@ -7,25 +7,25 @@
 # that was found in the original string and how many times it was found.
 
 def substrings(string, dictionary)
-  hash = {}
   string_array = string.scan(/\w+/)
+  hash = {}
 
   string_array.each do |word|
     dictionary.each do |w|
       next if word.downcase.scan(w).empty?
-      if hash.include?(w)
-        hash[w] += 1
-      else
-        hash[w] = 1
-      end
+      hash.include?(w) ? hash[w] += 1 : hash[w] = 1
     end
   end
 
-  puts hash
+  sorted_hash = hash.sort_by { |_word, count| count }.reverse
+  system "clear" or "cls"
+  puts "Input: #{string}\n\n"
+  puts "Matches:\n\n"
+  sorted_hash.each { |word, count| puts "- #{word}: #{count}" }
+  puts "\n"
 end
 
 dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i",
               "low", "own", "part", "partner", "sit"]
 
-substrings("below", dictionary)
 substrings("Howdy partner, sit down! How's it going?", dictionary)
