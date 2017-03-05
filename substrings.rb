28f2af6176f6020d069503325f98fcbc6eb880bf
lujanfernaud <lujanfernaud@gmail.com>
2017-03-05 20:58:31 +0000

    Initial implementation of substrings

--- substrings.rb
+++ substrings.rb
@@ -6,3 +6,22 @@
 # argument. It should return a hash listing each substring (case insensitive)
 # that was found in the original string and how many times it was found.
 
+def substrings(word, dictionary)
+  hash = {}
+
+  dictionary.each do |w|
+    next if word.scan(w).empty?
+    if hash.include?(w)
+      hash[w] += 1
+    else
+      hash[w] = 1
+    end
+  end
+
+  puts hash
+end
+
+dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i",
+              "low", "own", "part", "partner", "sit"]
+
+substrings("below", dictionary)
