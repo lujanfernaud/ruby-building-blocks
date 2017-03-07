d6542ed47e0ff3b4487108fee4bc4990439b82f7
lujanfernaud <lujanfernaud@gmail.com>
2017-03-07 20:33:42 +0000

    Start bubble_sort

---
+++ bubble_sort.rb
@@ -0,0 +1,17 @@
+# Project: Bubble Sort for The Odin Project
+# Author: Luján Fernaud
+#
+# Instructions:
+#
+# 1. Build a method #bubble_sort that takes an array and returns a
+# sorted array. It must use the bubble sort methodology (using #sort would be
+# pretty pointless, wouldn't it?).
+#
+# 2. Now create a similar method called #bubble_sort_by which sorts an array
+# but accepts a block. The block should take two arguments which represent
+# the two elements currently being compared. Expect that the block's return
+# will be similar to the spaceship operator you learned about before -- if
+# the result of the block is negative, the element on the left is "smaller"
+# than the element on the right. 0 means they are equal. A positive result means
+# the left element is greater. Use this to sort your array.
+
